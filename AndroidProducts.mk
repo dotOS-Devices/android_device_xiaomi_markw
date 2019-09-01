3abdc704f56c05483de638580fd0ce1fce0dc2a5
Granrage <vladon.borisenko@gmail.com>
2019-09-01 21:41:24 +0300

    markw: dot-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bootleg_markw.mk
+    $(LOCAL_DIR)/dot_markw.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo bootleg_markw-userdebug
+add_lunch_combo dot_markw-userdebug
